357a7ed8eed2f20ab890954e84896f08ac621ecd
Felix E. Klee <felix.klee@inka.de>
2020-05-11 17:30:20 +0800

    Add initial files

---
+++ README.md
@@ -0,0 +1,49 @@
+Introduction
+============
+
+This repo contains documentation for one of [our entries][2] to the *Mask Design
+Challenge by FabCafe Global.* ([screenshot][4]) It won the *FabCafe Prize.*
+
+![Photo series of Kristin wearing our mask][3]
+
+
+Hardware
+========
+
+  * ESP-EYE
+
+  * OLED: SSD1306, SPI ([Taobao screenshot][1])
+
+
+Firmware
+========
+
+Tested with:
+
+    ESP-IDF v4.2-dev-1415-ga2263571b
+
+To build (or extract existing build from archive):
+
+    $ cd esp/
+    $ idf.py build
+
+To flash, remove the display, then do:
+
+    $ idf.py -p /dev/ttyUSB0 flash --baud 2000000
+
+
+License
+=======
+
+Except where noted otherwise, files are licensed under the WTFPL.
+
+Copyright © 2020 [Felix E. Klee](felix.klee@inka.de)
+
+This work is free. You can redistribute it and/or modify it under the terms of
+the Do What The Fuck You Want To Public License, Version 2, as published by Sam
+Hocevar. See the COPYING file for more details.
+
+[1]: images/oled-on-taobao.png
+[2]: https://awrd.com/creatives/detail/9650105
+[3]: images/mask.jpg
+[4]: images/our-entry.png
---
+++ esp/main/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "ssd1306.h"
+#include "ssd1306_draw.h"
+#include "ssd1306_default_if.h"
+
+static const int SPIDisplayChipSelect = 15;
+static const int SPIDisplayWidth = 128;
+static const int SPIDisplayHeight = 64;
+static const int SPIResetPin = 2;
+
+struct SSD1306_Device SPIDisplay;
+
+bool DefaultBusInit() {
+    assert(SSD1306_SPIMasterInitDefault() == true);
+    assert(SSD1306_SPIMasterAttachDisplayDefault(&SPIDisplay, SPIDisplayWidth, SPIDisplayHeight, SPIDisplayChipSelect, SPIResetPin) == true);
+    return true;
+}
+
+void draw_line(int x0, int y0, int x1, int y1) {
+    SSD1306_DrawLine(&SPIDisplay, x0, y0, x1, y1, SSD_COLOR_WHITE);
+}
+
+void draw_emoji(const int lines[][4], const int number_of_lines) {
+    for (int i = 0; i < number_of_lines; i++) {
+        const int *line = lines[i];
+        draw_line(line[0], line[1], line[2], line[3]);
+    }
+
+    for (int i = 0; i < number_of_lines; i++) {
+        const int *line = lines[i];
+        draw_line(127 - line[0], line[1], 127 - line[2], line[3]);
+    }
+}
+
+void draw_happy() {
+    int lines[][4] =
+        {{0, 32, 16, 48},
+         {8, 40, 20, 28},
+         {20, 28, 34, 19},
+         {34, 19, 49, 14},
+         {49, 14, 64, 12}};
+    draw_emoji(lines, sizeof(lines) / sizeof(int[4]));
+}
+
+void draw_sad() {
+    int lines[][4] =
+        {
+         {8, 63 - 40, 20, 63 - 28},
+         {20, 63 - 28, 34, 63 - 19},
+         {34, 63 - 19, 49, 63 - 14},
+         {49, 63 - 14, 64, 63 - 12}
+        };
+    draw_emoji(lines, sizeof(lines) / sizeof(int[4]));
+}
+
+void draw_neutral() {
+    int lines[][4] =
+        {
+         {5, 28, 5, 36},
+         {5, 32, 122, 32}
+        };
+    draw_emoji(lines, sizeof(lines) / sizeof(int[4]));
+}
+
+void delay(
+           int x // ms
+           ) {
+    vTaskDelay(x / portTICK_PERIOD_MS);
+}
+
+void app_main(void) {
+    if (!DefaultBusInit()) {
+        return;
+    }
+
+    for (unsigned int i = 0;; i++) {
+        SSD1306_Clear(&SPIDisplay, SSD_COLOR_BLACK);
+        switch (i % 3) {
+        case 0:
+            draw_happy();
+            break;
+        case 1:
+            draw_neutral();
+            break;
+        case 2:
+            draw_sad();
+            break;
+        }
+        SSD1306_Update(&SPIDisplay);
+        delay(1000);
+    }
+}
